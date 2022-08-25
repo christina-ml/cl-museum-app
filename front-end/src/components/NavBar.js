d574343d156bbe2476694623252f81510d292375
Christina Loiacono <loiaconochristina@gmail.com>
2022-08-24 21:32:26 -0400

    aligned navbar to top of page

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
         <nav className="navbar navbar-light" bg="light" expand="lg">
             <Link to="/">
                 <h2>
-                    <img height="30" className="d-inline-block align-top" src="https://res.cloudinary.com/dk-find-out/image/upload/q_70,c_pad,w_1200,h_630,f_auto/DCTM_Penguin_UK_DK_AL503037_j4wz4v.jpg" alt="fossil" />
+                    <img src="https://res.cloudinary.com/dk-find-out/image/upload/q_70,c_pad,w_1200,h_630,f_auto/DCTM_Penguin_UK_DK_AL503037_j4wz4v.jpg" height="30" class="d-inline-block align-top" alt="fossil" />
                     <span>Dinosaur Museum</span>
                 </h2>
             </Link>
